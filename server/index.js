964b42ccaf4c6e460a19fa0584a4cb86a37a9984
Teinturier <henri.teinturier@gmail.com>
2025-02-22 01:10:07 +0100

    Remove manual CORS middleware from server configuration

--- server/index.js
+++ server/index.js
@@ -14,23 +14,6 @@ app.use(express.static("../dist"));
 app.use(express.json());
 app.use(cookieParser());
 
-// Ajout des headers CORS manuellement
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Accepte toutes les origines
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Cookie"
-  );
-  // On retire temporairement Allow-Credentials car il n'est pas compatible avec Allow-Origin: *
-  // res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 app.use(router);
 
 app.get("*", (req, res) => {
